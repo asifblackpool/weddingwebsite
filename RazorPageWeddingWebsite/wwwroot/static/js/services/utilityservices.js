56ca2886388e2fcc797f539c488ae068622c7f66
Asif Nissar <asif.nissar@blackpool.gov.uk>
2025-09-03 13:43:33 +0100

    fix contaact form styling

---
+++ RazorPageWeddingWebsite/wwwroot/static/js/services/utilityservices.js
@@ -0,0 +1,16 @@
+﻿const utilityservices = (function () {
+    // private function
+    function replaceCommasInParagraph(selector) {
+        const p = document.querySelector(selector);
+        if (!p) return;
+
+        const text = p.textContent || "";
+        const updated = text.replace(/,\s*/g, "<br/>"); // replace commas with <br/>
+        p.innerHTML = updated;
+    }
+
+    // public API
+    return {
+        replaceCommasInParagraph: replaceCommasInParagraph
+    };
+})();
