4763599ba45d7e086992b2fabebb61baa1b5b7ea
Asif Nissar <asif.nissar@blackpool.gov.uk>
2025-10-01 20:48:47 +0100

    fix mobile menu to be closer to design

--- RazorPageWeddingWebsite/wwwroot/static/js/services/menuservices.js
+++ RazorPageWeddingWebsite/wwwroot/static/js/services/menuservices.js
@@ -4,11 +4,15 @@
       const selectors = {
         menuToggle: ".mobile-menu-toggle",
         navList: ".nav-list",
-        submenuLinks: ".nav-item.has-panel > a"
+        submenuLinks: ".nav-item.has-panel > span.caret"
      };
 
-     _menuCaret = null;
-     _munuSubParent = null;
+     _menuCaret         = null;
+     _munuSubParent     = null;
+     _parent            = null;
+     _nearestLink       = null;
+     _main              = null;
+     _homeTitle         = null;
 
      var setupCaret = function (caret) {
 
@@ -17,6 +21,7 @@
      var closeSubMenu = function () {
          if (_parent.classList.contains("open")) {
              _parent.classList.toggle("open");
+             _nearestLink.style.color = "";
          } 
      };
 
@@ -34,11 +39,21 @@
              const isOpen = navList.style.display === "flex";
              navList.style.display = isOpen ? "none" : "flex";
              menuToggle.classList.toggle("active", !isOpen);
+
+             _main = document.querySelector("main");
+             _homeTitle = document.querySelector("h1.home");
+
              if (!isOpen) {
+                 //we are opening the menu
                  menuToggle.classList.add("active");
+                 _main.style.opacity = "0.1";
+                 _homeTitle.style.opacity = "0.3";
              }
              else {
+                 //We are closing the menu
                  menuToggle.classList.remove("active");
+                 _main.style.opacity = "1";
+                 _homeTitle.style.opacity = "1";
                  closeSubMenu();
              }
          
@@ -50,8 +65,18 @@
                  if (window.innerWidth <= 576) {
                      e.preventDefault();
                      _parent = link.parentElement;
+            
                      const caret = link.querySelector(".caret");
                      _parent.classList.toggle("open");
+
+                     let navItem = link.closest(".nav-item");    // get the parent li
+                     _nearestLink = navItem.querySelector("a");    
+                     if (_parent.classList.contains("open")) {
+                         _nearestLink.style.color = "#dd3394";
+                     } else {
+                         _nearestLink.style.color = "";
+                     }
+                 
                      
                  }
              });
